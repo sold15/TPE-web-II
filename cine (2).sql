74a50395bdb995889baf7cfe8f1fd23cf679589a
SolanaDoñate <soldonate15@gmail.com>
2023-09-26 21:47:58 -0300

    cambios2

---
+++ cine (2).sql
@@ -0,0 +1,129 @@
+ -- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 26-09-2023 a las 23:43:57
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `cine`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `genero`
+--
+
+CREATE TABLE `genero` (
+  `ID` int(11) NOT NULL,
+  `nombre` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `genero`
+--
+
+INSERT INTO `genero` (`ID`, `nombre`) VALUES
+(1, 'drama'),
+(3, 'romance'),
+(5, 'ciencia ficcion'),
+(6, 'terror');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `peliculas`
+--
+
+CREATE TABLE `peliculas` (
+  `ID` int(11) NOT NULL,
+  `Titulo` varchar(200) NOT NULL,
+  `AñoEstreno` date NOT NULL,
+  `Duracion` varchar(50) NOT NULL,
+  `id_genero` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `peliculas`
+--
+
+INSERT INTO `peliculas` (`ID`, `Titulo`, `AñoEstreno`, `Duracion`, `id_genero`) VALUES
+(1, 'la vida es bella', '0000-00-00', '1h 56m ', 1),
+(2, 'la vida es bella', '0000-00-00', '1h 56m ', 1),
+(3, 'orgullo y prejuicio', '0000-00-00', '2h 7m', 1),
+(4, 'orgullo y prejuicio', '0000-00-00', '2h 7m', 1),
+(5, 'cartas a julieta', '0000-00-00', '1h 45m', 3),
+(6, 'cartas a julieta', '0000-00-00', '1h 45m', 3),
+(7, 'recuerdame', '0000-00-00', '1h 53m', 3),
+(8, 'recuerdame', '0000-00-00', '1h 53m', 3),
+(9, 'los juegos del hambre', '0000-00-00', '2h 22m', 5),
+(10, 'los juegos del hambre', '0000-00-00', '2h 22m', 5),
+(11, 'king kong', '0000-00-00', '3h 7m', 5),
+(12, 'king kong', '0000-00-00', '3h 7m', 5),
+(13, 'la noche del demonio', '0000-00-00', '1h 43m', 6),
+(14, 'la noche del demonio', '0000-00-00', '1h 43m', 6),
+(15, 'el niño', '0000-00-00', '1h 37m', 6),
+(16, 'el niño', '0000-00-00', '1h 37m', 6);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `genero`
+--
+ALTER TABLE `genero`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indices de la tabla `peliculas`
+--
+ALTER TABLE `peliculas`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `id_genero` (`id_genero`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `genero`
+--
+ALTER TABLE `genero`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT de la tabla `peliculas`
+--
+ALTER TABLE `peliculas`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `peliculas`
+--
+ALTER TABLE `peliculas`
+  ADD CONSTRAINT `peliculas_ibfk_1` FOREIGN KEY (`id_genero`) REFERENCES `genero` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `peliculas_ibfk_2` FOREIGN KEY (`id_genero`) REFERENCES `genero` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `peliculas_ibfk_3` FOREIGN KEY (`id_genero`) REFERENCES `genero` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
